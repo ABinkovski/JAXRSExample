205cfcc5be6db1734e804e514331a3826721494e
abinkovski <alexbine@gmail.com>
2023-12-07 15:16:47 +0200

    minor fixes

--- SpringBootExample/src/main/java/com/edu/services/TestApiNoInterfaceV2.java
+++ SpringBootExample/src/main/java/com/edu/services/TestApiNoInterfaceV2.java
@@ -1,16 +1,15 @@
 package com.edu.services;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(path = "/v2")
 public class TestApiNoInterfaceV2 {
 
     @GetMapping("/")
     public String sayHi() {
-        return "Hello v1" + this.getClass().getSimpleName();
+        return "Hello v2" + this.getClass().getSimpleName();
     }
 }
