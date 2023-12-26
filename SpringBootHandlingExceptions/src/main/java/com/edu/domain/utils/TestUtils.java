87db9be96355b8c23c550ede313e857dd58759c9
abinkovski <alexbine@gmail.com>
2023-12-26 17:33:14 +0200

    Add big stack trace handled exception

--- SpringBootHandlingExceptions/src/main/java/com/edu/domain/utils/TestUtils.java
+++ SpringBootHandlingExceptions/src/main/java/com/edu/domain/utils/TestUtils.java
@@ -10,7 +10,7 @@ public class TestUtils {
 
     private static IllegalArgumentException getException(final int level, final int expected) {
         if (level >= expected) {
-            throw new IllegalArgumentException(LocalDateTime.now().toString());
+            return new IllegalArgumentException(LocalDateTime.now().toString());
         }
         return getException(level + 1, expected);
     }
--- SpringBootHandlingExceptions/src/main/java/com/edu/services/RootApiService.java
+++ SpringBootHandlingExceptions/src/main/java/com/edu/services/RootApiService.java
@@ -7,22 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.edu.domain.utils.TestUtils.getException;
+
 @RestController("anyCustomServiceBeanName")
 @RequestMapping("/api")
 public class RootApiService {
 
     @GetMapping("/ex1")
     public String itShouldFail() {
-        // Exception is not handled on any not null cause
-//        throw new CustomException("this is test exception", getException(100)); // stackTrace 152
-//        throw new CustomException("this is test exception", getException(90)); // stackTrace 142
-//        throw new CustomException("this is test exception", getException(80)); // stackTrace 132
-//        throw new CustomException("this is test exception", getException(70)); // stackTrace 122
-//        throw new CustomException("this is test exception", getException(10)); // stackTrace 62
-//        throw new CustomException("this is test exception", getException(0)); // stackTrace 62
-//        throw new CustomException("this is test exception", new CustomException("", getException(1))); // stackTrace 62
         throw new CustomException("this is test exception"); // stackTrace 62
     }
+
+
+    @GetMapping("/handledWithBigStackTrace")
+    public String handledWithBigStackTrace() {
+        throw new CustomException("this is test exception", getException(400));
+    }
+
 
 
     @GetMapping("/ex2")
